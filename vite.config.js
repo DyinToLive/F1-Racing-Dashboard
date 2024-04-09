e10f1e40d9e9145ee7c93e5ea45b7dc922c7429f
DyinToLive <88452454+DyinToLive@users.noreply.github.com>
2024-04-08 22:03:40 -0600

    ee

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        // split vendor code into separate chunks
+                        return 'vendor';
+                    }
+                }
+            }
+        },
+        chunkSizeWarningLimit: 600, // Adjust the chunk size warning limit (in KB)
+    },
+});
