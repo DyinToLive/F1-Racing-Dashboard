b2401bb1a3677095f87b9cccd97e9f638ccfba00
DyinToLive <88452454+DyinToLive@users.noreply.github.com>
2024-04-08 22:19:02 -0600

    syntax

--- vite.config.js
+++ vite.config.js
@@ -15,8 +15,6 @@ export default defineConfig({
             }
         },
         chunkSizeWarningLimit: 600, 
-        build: {
-            outDir: 'dist', 
-          },
+        outDir: 'dist', 
     },
 });
